ac83461411337a5ffe83e723fa4bb0a1fe3581dd
Jacob Goodman <jswag7890@gmail.com>
2024-03-01 00:13:45 -0800

    made pitcher data only for last season

--- fantasy_baseball_ml_backend/fantasy_baseball_ml/views.py
+++ fantasy_baseball_ml_backend/fantasy_baseball_ml/views.py
@@ -12,7 +12,7 @@ def player_stats(request):
     player_id = player.iloc[0]['key_mlbam']
     first = int(player.iloc[0]['mlb_played_first'])
     last = int(player.iloc[0]['mlb_played_last'])
-    player_stats = statcast_pitcher(str(first)+'-01-01', str(last)+'-12-31', player_id)
+    player_stats = statcast_pitcher('2023'+'-03-30', str(last)+'-12-31', player_id)
 
     player_stats_cleaned = player_stats.where(pd.notnull(player_stats), None)
     mean_speeds = player_stats_cleaned.groupby("pitch_type").release_speed.agg("mean")
